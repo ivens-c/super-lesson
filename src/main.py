cfb1eaf3e2ad52ccaddce1e3c83f7c65cfe53d59
ivens-c <ivens-c@ya.ru>
2023-06-05 15:25:40 +0300

    add some files fo test gitignore

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import modules.perimeter as perimeter
 import modules.factorial as factorial
+import modules.tests.test_all as ta
 print(perimeter.perimeter_func([1, 2, 3]))
 print(factorial.factorial_rec(5))
 print(factorial.factorial_normal(5))
